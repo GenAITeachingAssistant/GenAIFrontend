03d9825e2e02a02aab134486160a24085157ea21
Karuna Singhal <singhalkaruna73@gmail.com>
2024-10-02 12:51:50 +0530

    page not found and avatar api integration

--- src/pages/Avatar/Avatar.jsx
+++ src/pages/Avatar/Avatar.jsx
@@ -1,11 +1,45 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import Button from "components/Button/Button";
+import restClient from "restClient";
 
 import { avatars } from "constants/avatars";
 import wave from "assets/svg/wave.svg";
-import Button from "components/Button/Button";
 
 function Avatar() {
+  const userData = JSON.parse(localStorage.getItem("userData"));
+  const id = userData._id;
+  const navigate = useNavigate();
+
   const [selectedAvatar, setSelectedAvatar] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const setAvatar = async () => {
+    try {
+      setIsLoading(true);
+
+      const { data } = await restClient({
+        method: "POST",
+        url: `/auth/setAvatar/${id}`,
+        data: { avatar: selectedAvatar },
+      });
+
+      if (data.success) {
+        localStorage.setItem("userData", JSON.stringify(data.userData));
+
+        toast.success("Avatar set successfully");
+
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <div className="flex flex-col justify-between h-[100vh]">
@@ -20,17 +54,20 @@ function Avatar() {
               src={avatar}
               alt=""
               className={`cursor-pointer ${
-                selectedAvatar === avatar ? "border-4 border-primary-black" : ""
+                selectedAvatar === `avatar${index + 1}`
+                  ? "border-4 border-primary-black"
+                  : ""
               } rounded-full w-[10rem] h-[10rem]`}
-              onClick={() => setSelectedAvatar(avatar)}
+              onClick={() => setSelectedAvatar(`avatar${index + 1}`)}
             />
           ))}
         </div>
         <div className="mt-16">
           <Button
             text="Select an Avatar to Continue"
-            onClick={() => console.log(selectedAvatar)}
-            disabled={!selectedAvatar}
+            onClick={setAvatar}
+            disabled={isLoading || !selectedAvatar}
+            isLoading={isLoading}
           />
         </div>
       </div>
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import { ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
+
+import wave from "assets/svg/wave.svg";
+import notFound from "assets/images/notFound.jpg";
+
 function NotFound() {
-  return <div></div>;
+  const navigate = useNavigate();
+  return (
+    <div className="flex flex-col h-[100vh] justify-between overflow-hidden">
+      <div className="flex gap-[1rem] items-center justify-around">
+        <img src={notFound} alt="notFound" className="w-[32rem] h-[30rem]" />
+        <div className="flex flex-col items-center gap-2">
+          <span className="text-primary-teal text-[4rem] font-bold">
+            Page Not Found
+          </span>
+          <p className="text-secondary-black">
+            We're sorry the page you required could not be found
+          </p>
+          <button
+            className="flex gap-[0.8rem] items-center text-[1.4rem] font-semibold bg-primary-black text-primary-white px-4 py-2 rounded-[1rem] mt-6"
+            onClick={() => navigate(-1)}
+          >
+            <ArrowUturnLeftIcon width={20} />
+            <span>Go Back</span>
+          </button>
+        </div>
+      </div>
+      <img src={wave} alt="" />
+    </div>
+  );
 }
 
 export default NotFound;
